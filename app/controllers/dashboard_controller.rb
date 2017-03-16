82af965557d81fb3f9d056eea5ec9888d472c290
IgorPolyakov <igorpolyakov@protonmail.com>
2017-03-16 13:14:34 +0700

    dashboarh view status from DB

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    @check_count = Check.count
+    @report_count = Report.count
+    @organization_count = Organization.count
+    @inspector_count = Inspector.count
+
   end
 end
