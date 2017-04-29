75b5edb2da277b3c46e13b98e3d3e1a29b08d8e1
IgorPolyakov <igorpolyakov@protonmail.com>
2017-04-29 15:26:30 +0700

    comment last migration && add I18n

--- db/schema.rb
+++ db/schema.rb
@@ -10,93 +10,83 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428083715) do
+ActiveRecord::Schema.define(version: 20170428102428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "checks", force: :cascade do |t|
-    t.integer  "amount"
-    t.string   "checks_number"
-    t.date     "deadline"
-    t.string   "reason"
-    t.string   "category"
-    t.string   "violation"
-    t.string   "response_measure"
-    t.string   "direction"
-    t.string   "term"
-    t.string   "case_number"
-    t.integer  "number_of_volumes"
-    t.boolean  "for_office"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.integer  "inspector_id"
-    t.integer  "organization_id"
-    t.index ["inspector_id"], name: "index_checks_on_inspector_id", using: :btree
-    t.index ["organization_id"], name: "index_checks_on_organization_id", using: :btree
+  create_table "checks", id: :serial, force: :cascade do |t|
+    t.integer "amount"
+    t.string "checks_number"
+    t.date "deadline"
+    t.string "reason"
+    t.string "category"
+    t.string "violation"
+    t.string "response_measure"
+    t.string "direction"
+    t.string "term"
+    t.string "case_number"
+    t.integer "number_of_volumes"
+    t.boolean "for_office"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "inspector"
+    t.integer "organization_id"
+    t.index ["inspector"], name: "index_checks_on_inspector"
+    t.index ["organization_id"], name: "index_checks_on_organization_id"
   end
 
   create_table "checks_and_organizations", id: false, force: :cascade do |t|
     t.integer "check_id"
     t.integer "organization_id"
-    t.index ["check_id"], name: "index_checks_and_organizations_on_check_id", using: :btree
-    t.index ["organization_id"], name: "index_checks_and_organizations_on_organization_id", using: :btree
+    t.index ["check_id"], name: "index_checks_and_organizations_on_check_id"
+    t.index ["organization_id"], name: "index_checks_and_organizations_on_organization_id"
   end
 
   create_table "checks_people", id: false, force: :cascade do |t|
     t.integer "check_id"
     t.integer "person_id"
-    t.index ["check_id"], name: "index_checks_people_on_check_id", using: :btree
-    t.index ["person_id"], name: "index_checks_people_on_person_id", using: :btree
   end
 
-  create_table "inspectors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "position"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "last_name"
-    t.string   "middle_name"
+  create_table "inspectors", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "middle_name"
   end
 
-  create_table "organizations", force: :cascade do |t|
-    t.string   "company_name"
-    t.string   "first_name"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "middle_name"
-    t.string   "second_name"
-    t.string   "position"
+  create_table "organizations", id: :serial, force: :cascade do |t|
+    t.string "company_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "middle_name"
+    t.string "second_name"
+    t.string "position"
   end
 
-  create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "middle_name"
+  create_table "people", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "middle_name"
   end
 
-  create_table "reports", force: :cascade do |t|
-    t.integer  "number"
+  create_table "reports", id: :serial, force: :cascade do |t|
+    t.integer "number"
     t.datetime "dead_line"
-    t.integer  "inspector_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "reason"
-    t.string   "result"
-    t.index ["inspector_id"], name: "index_reports_on_inspector_id", using: :btree
+    t.integer "inspector_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reason"
+    t.string "result"
+    t.index ["inspector_id"], name: "index_reports_on_inspector_id"
   end
 
-  create_table "supervisors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "middle_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_foreign_key "checks", "inspectors"
+  add_foreign_key "checks", "inspectors", column: "inspector"
   add_foreign_key "checks", "organizations"
   add_foreign_key "reports", "inspectors"
 end
