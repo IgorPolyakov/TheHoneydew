a6d61366a380ae6615e43bb807b451c9aa9e72ec
IgorPolyakov <igorpolyakov@protonmail.com>
2017-05-04 02:01:02 +0700

    add ru locale

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module TheHoneydew
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
   end
 end
