f5ff4ee8eac72d8e6aaf8e95d84c016f5ee31e73
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-05-17 16:49:35 +0700

    Update inspector.rb

--- app/models/inspector.rb
+++ app/models/inspector.rb
@@ -1,5 +1,5 @@
 class Inspector < ApplicationRecord
 	validates :first_name, :last_name, :position, :middle_name, presence: true
-	has_many :checks
-	has_many :reports
+	has_many :checks, dependent: :destroy
+	has_many :reports, dependent: :destroy
 end
