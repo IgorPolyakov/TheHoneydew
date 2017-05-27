c839f758e442e1272d4ca39b62d6c8a45f487b3d
Vexarr <dveliar@mail.ru>
2017-05-27 13:52:39 +0700

    Delete one migrate

--- db/migrate/20170513114819_add_inspector_to_reports.rb
+++
@@ -1,5 +0,0 @@
-class AddInspectorToReports < ActiveRecord::Migration[5.0]
-  def change
-    # add_reference :reports, :inspector, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,68 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_513_114_819) do
+ActiveRecord::Schema.define(version: 20170513114819) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'checks', force: :cascade do |t|
-    t.integer  'amount'
-    t.string   'checks_number'
-    t.date     'deadline'
-    t.string   'reason'
-    t.string   'category'
-    t.string   'violation'
-    t.string   'response_measure'
-    t.string   'direction'
-    t.string   'term'
-    t.string   'case_number'
-    t.integer  'number_of_volumes'
-    t.boolean  'for_office'
-    t.datetime 'created_at',        null: false
-    t.datetime 'updated_at',        null: false
-    t.integer  'inspector_id'
-    t.index ['inspector_id'], name: 'index_checks_on_inspector_id', using: :btree
+  create_table "checks", force: :cascade do |t|
+    t.integer  "amount"
+    t.string   "checks_number"
+    t.date     "deadline"
+    t.string   "reason"
+    t.string   "category"
+    t.string   "violation"
+    t.string   "response_measure"
+    t.string   "direction"
+    t.string   "term"
+    t.string   "case_number"
+    t.integer  "number_of_volumes"
+    t.boolean  "for_office"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "inspector_id"
+    t.index ["inspector_id"], name: "index_checks_on_inspector_id", using: :btree
   end
 
-  create_table 'inspectors', force: :cascade do |t|
-    t.string   'first_name'
-    t.string   'position'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.string   'last_name'
-    t.string   'middle_name'
+  create_table "inspectors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "position"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "last_name"
+    t.string   "middle_name"
   end
 
-  create_table 'organizations', force: :cascade do |t|
-    t.string   'company_name'
-    t.string   'first_name'
-    t.datetime 'created_at',   null: false
-    t.datetime 'updated_at',   null: false
-    t.string   'middle_name'
-    t.string   'last_name'
-    t.string   'position'
+  create_table "organizations", force: :cascade do |t|
+    t.string   "company_name"
+    t.string   "first_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "position"
   end
 
-  create_table 'people', force: :cascade do |t|
-    t.string   'first_name'
-    t.string   'last_name'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.string   'middle_name'
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "middle_name"
   end
 
-  create_table 'reports', force: :cascade do |t|
-    t.string   'number'
-    t.date     'deadline'
-    t.integer  'inspector_id'
-    t.string   'executive'
-    t.string   'reason'
-    t.string   'result'
-    t.datetime 'created_at',   null: false
-    t.datetime 'updated_at',   null: false
-    t.index ['inspector_id'], name: 'index_reports_on_inspector_id', using: :btree
+  create_table "reports", force: :cascade do |t|
+    t.string   "number"
+    t.date     "deadline"
+    t.integer  "inspector_id"
+    t.string   "executive"
+    t.string   "reason"
+    t.string   "result"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["inspector_id"], name: "index_reports_on_inspector_id", using: :btree
   end
 
-  add_foreign_key 'checks', 'inspectors'
-  add_foreign_key 'reports', 'inspectors'
+  add_foreign_key "checks", "inspectors"
+  add_foreign_key "reports", "inspectors"
 end
