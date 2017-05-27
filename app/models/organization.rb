4cb0e954ad933685dd37efae601ef01d01dd24a1
Vexarr <dveliar@mail.ru>
2017-05-27 15:15:13 +0700

    Add forein key organization to reports

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,4 @@
 class Organization < ApplicationRecord
   validates :first_name, :last_name, :position, :company_name, :middle_name, presence: true
+  has_many :reports, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513114819) do
+ActiveRecord::Schema.define(version: 20170527080520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,11 +68,14 @@ ActiveRecord::Schema.define(version: 20170513114819) do
     t.string   "executive"
     t.string   "reason"
     t.string   "result"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "organization_id"
     t.index ["inspector_id"], name: "index_reports_on_inspector_id", using: :btree
+    t.index ["organization_id"], name: "index_reports_on_organization_id", using: :btree
   end
 
   add_foreign_key "checks", "inspectors"
   add_foreign_key "reports", "inspectors"
+  add_foreign_key "reports", "organizations"
 end
