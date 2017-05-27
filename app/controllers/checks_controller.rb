c84c153106d19b0d44f072a56702098f1d4b8212
IgorPolyakov <igorpolyakov@protonmail.com>
2017-05-27 20:43:12 +0700

    fix passwd

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -70,6 +70,6 @@ class ChecksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def check_params
-    params.require(:check).permit(:amount, :checks_number, :deadline, :reason, :category, :violation, :response_measure, :direction, :term, :case_number, :number_of_volumes, :for_office, :supervisor_id, :visor_id)
+    params.require(:check).permit(:amount, :checks_number, :deadline, :reason, :category, :violation, :response_measure, :direction, :term, :case_number, :number_of_volumes, :for_office, :executive, :inspector_id)
   end
 end
