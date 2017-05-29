6bcc1f5f9303f98def2e442515c942eb50f43092
Daniil <layz100@gmail.com>
2017-05-29 12:40:38 +0700

    add to migration new field, fix edit and delete button html

--- db/migrate/20170427131431_devise_create_users.rb
+++ db/migrate/20170427131431_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :is_admin, default: 0, null: false
 
       ## Recoverable
       #t.string   :reset_password_token
@@ -35,7 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    #add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
