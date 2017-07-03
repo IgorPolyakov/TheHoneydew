1a9d1a50846a7e30747a020adda8565267fea76b
Daniil <layz100@gmail.com>
2017-07-03 19:40:30 +0700

    closed #23: see routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :checks
-  resources :people
+
+  authenticate :user, lambda {|u| u.is_admin == true} do
+    resources :checks
+    resources :people
+    resources :organizations
+    resources :reports
+    resources :inspectors
+  end
+
+  resources :checks, only: [:index,:show]
+  resources :people, only: [:index,:show]
   get 'dashboard/index'
 
   root 'dashboard#index'
-  resources :organizations
-  resources :reports
-  resources :inspectors
+  resources :organizations, only: [:index,:show]
+  resources :reports, only: [:index,:show]
+  resources :inspectors, only: [:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
