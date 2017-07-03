060a655c4ecf70d45152f16bccb20d3571218082
Daniil <layz100@gmail.com>
2017-07-03 16:20:06 +0700

    Remove unused method

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,3 @@ module TheHoneydew
     # config.force_ssl = true
   end
 end
-
-
-def authenticate_user!
-	if user_signed_in?
-	  super
-	else
-	  redirect_to new_user_session_path	, :notice => 'Sign in!'
-	end
-end
