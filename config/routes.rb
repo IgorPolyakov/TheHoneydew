4ab0429498a6f9f4faa6c54dd890126a690aaf9b
Daniil <layz100@gmail.com>
2017-07-03 17:56:01 +0700

    /sign_in is working now

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :reports
   resources :inspectors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do get '/sign_in' => 'devise/sessions#new' end
+
 end
