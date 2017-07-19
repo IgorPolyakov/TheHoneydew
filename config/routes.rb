aab1e8688946e499c0a70b3c3fc6800c3708001f
IgorPolyakov <igorpolyakov@protonmail.com>
2017-07-19 14:13:21 +0700

    cleaning by rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-  
   get 'dashboard/report'
 
-  devise_for :users, skip: [:sessions] 
+  devise_for :users, skip: [:sessions]
   unauthenticated do
     as :user do
       get '', to: 'devise/sessions#new', as: :new_user_session
       post '', to: 'devise/sessions#create', as: :user_session
     end
   end
-  
+
   authenticated do
     as :user do
       delete '', to: 'devise/sessions#destroy', as: :destroy_user_session
     end
   end
 
-  authenticate :user, lambda {|u| u.is_admin == true} do
+  authenticate :user, ->(u) { u.is_admin == true } do
     resources :checks
     resources :people
     resources :organizations
@@ -24,16 +23,13 @@ Rails.application.routes.draw do
     resources :inspectors
   end
 
-  resources :checks, only: [:index,:show]
-  resources :people, only: [:index,:show]
+  resources :checks, only: %i[index show]
+  resources :people, only: %i[index show]
   get 'dashboard/index'
 
   root 'dashboard#index'
-  resources :organizations, only: [:index,:show]
-  resources :reports, only: [:index,:show]
-  resources :inspectors, only: [:index,:show]
+  resources :organizations, only: %i[index show]
+  resources :reports, only: %i[index show]
+  resources :inspectors, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  
-
 end
