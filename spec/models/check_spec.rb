19f862d904f58caba2b871d8cee83f44731607c7
IgorPolyakov <igorpolyakov@protonmail.com>
2017-08-01 01:15:55 +0700

    fix #55 update test && update travis yaml

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Check, type: :model do
   some_check = Check.new(amount: 10)
   it 'amount should be greater than or equal to one' do
-    expect(some_check).to be_valid
+    expect(some_check.amount).to eq(10)
   end
 end
