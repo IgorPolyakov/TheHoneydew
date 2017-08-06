e602152a82f166cbd4687a0f7cb75a75a1e1ffa6
IgorPolyakov <igorpolyakov@protonmail.com>
2017-08-07 01:39:56 +0700

    update #2: add report test

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,10 @@
 class Report < ApplicationRecord
+  def remove_quote(obj, str)
+    obj.gsub!(/[\[\]\"]/, '') if attribute_present?(str)
+  end
   before_save do
-    result.gsub!(/[\[\]\"]/, '') if attribute_present?('result')
-    reason.gsub!(/[\[\]\"]/, '') if attribute_present?('reason')
+    remove_quote(reason, 'reason')
+    remove_quote(result, 'result')
   end
   belongs_to :inspector # , dependent: :destroy
   belongs_to :organization # , dependent: :destroy иначе удаляет больше одной записи
