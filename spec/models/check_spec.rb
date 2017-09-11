d1f25a2f99e796f04988dbc1e030bf6f9ca8ddce
IgorPolyakov <igorpolyakov@protonmail.com>
2017-09-12 00:37:21 +0700

    Add 'Check' test (update #2)

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
-
+require 'date'
 RSpec.describe Check, type: :model do
   some_check = Check.new(amount: 10)
   it 'amount should be greater than or equal to one' do
     expect(some_check.amount).to eq(10)
+  end
+   it 'check start should less than check finish' do
+    some_check.check_start = Date.today
+    some_check.check_finish = Date.today
+    some_check.check_remark_destroy = Date.today
+    expect(some_check.valid?).to be_falsey
   end
 end
