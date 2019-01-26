364b960bba8ef2d061b06521b6feceb4ce278b0c
Nikel <vova_niemisto@mail.ru>
2019-01-26 13:58:14 +0700

    close #104: map is working

--- app/controllers/custom_sessions_controller.rb
+++ app/controllers/custom_sessions_controller.rb
@@ -1,13 +1,24 @@
 require 'json'
-
+require 'ipstack/api'
 
 class CustomSessionsController < Devise::SessionsController
 	def create
 		super 
 		puts "\n\n‿︵‿ヽ(°□° )ノ︵‿︵Получилось!‿︵‿ヽ(°□° )ノ︵‿︵\n"
-		#Ipstack::API.check
-		p current_user.current_sign_in_ip
+		ip = current_user.current_sign_in_ip.to_s
+		result = Ipstack::API.standard(ip)
+		data_hash = JSON.parse(result.to_json)
+		if (data_hash['latitude'] != nil) or (data_hash['longitude'] != nil)
+			latitude = data_hash['latitude'].to_f
+			longitude = data_hash['longitude'].to_f
+		else 
+			latitude = 0
+			longitude = 0
+		end
 
+		p latitude
+		p longitude
+		current_user.update(latitude_sign_in: latitude,longitude_sign_in: longitude)
 
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,40 +11,5 @@ class User < ApplicationRecord
  #has_many :reports#, dependent: :destroy and :edit
  #has_many :people#, dependent: :destroy and :edit
  #has_many :organizations#, dependent: :destroy and :edit
-#=begin
- 	def lantitude
- 		make_geo(user.ip, "lan")
- 	end
 
- 	def longtitude
- 		make_geo(user.ip, "lon")
- 	end
-
- 	private
-
- 	def make_geo(ip_adress, lan_or_lon)
- 		#тут код с преобразованиями
- 		if (lan_or_lon == "lan")
-	 		uri = URI('http://api.ipstack.com/check
-	 			? access_key = d7349c126fc66c4abde362e4a04ce4f1
-	 			& output = json')
-			req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-			req.body = {param1: '', param2: ''}.to_json
-			res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-				http.request(req)
-			end
-	    	puts "response #{res.body}"
-	    elsif (lan_or_lon == "lon")
-	    	uri = URI('http://api.ipstack.com/check
-	 			? access_key = d7349c126fc66c4abde362e4a04ce4f1
-	 			& output = json')
-			req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-			req.body = {param1: '', param2: ''}.to_json
-			res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-				http.request(req)
-			end
-	    	puts "response #{res.body}"
-	    end
- 	end
-#=end
 end
